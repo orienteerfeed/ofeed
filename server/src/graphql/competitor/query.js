30327aa8bfd2dbb0871ce89217e6cc846092e880
lukaskett <k.lukas15@seznam.cz>
2025-11-03 23:11:54 +0100

    fixed graphql query to return also class and splits

--- server/src/graphql/competitor/query.js
+++ server/src/graphql/competitor/query.js
@@ -4,6 +4,24 @@ import { getCompetitorsByClass } from './shared.js';
 export const competitorById = (_, { id }, context) => {
   return prisma.competitor.findUnique({
     where: { id: id },
+    include: {
+      splits: {
+        select: {
+          controlCode: true,
+          time: true,
+        },
+      },
+      class: {
+        select: {
+          id: true,
+          externalId: true,
+          name: true,
+          length: true,
+          climb: true,
+          controlsCount: true,
+        },
+      },
+    },
   });
 };
 export const competitorsByClass = (_, { id }, context) => {
